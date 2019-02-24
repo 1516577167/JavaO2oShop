70bb23d81191d8b8cbf8936c6d9603a19784479f
qxb <que350822@qq.com>
2019-02-24 16:22:28 +0800

    delAll

--- o2o/src/main/java/com/o2o/service/CacheService.java
+++
@@ -1,7 +0,0 @@
-package com.o2o.service;
-
-public interface CacheService {
-
-	
-	void removeFromCache(String keyPrefix);
-}
--- o2o/src/main/java/com/o2o/service/PerInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.o2o.service;
-
-
-import com.o2o.entity.PersonInfo;
-
-public interface PerInfoService {
-	PersonInfo queryPersonInfo(Long userId);
-
-}
